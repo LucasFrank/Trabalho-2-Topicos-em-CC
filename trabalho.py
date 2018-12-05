6583daf14e09cbd12b4bf9966fc1cd12fb18301f
LucasFrank <lucas.r.frank@gmail.com>
2018-12-05 17:37:00 -0200

    Novo método

--- trabalho.py
+++ trabalho.py
@@ -4,73 +4,61 @@ import numpy as np
 import math as m
 
 def exata(x,alpha,t,case):
-    y = np.zeros(len(x))
+    y = np.empty(len(x))
     if case == 1:
-        for i in range(Nx+1):
-            if x[i] < 0.2:
-                y[i] = m.exp(-m.log(50,10) * ((((x[i] - alpha * t) - 0.15)/0.05) ** 2))
-            elif x[i] > 0.3 and x[i] < 0.4:
+        for i in range(len(x)):
+            y[i] = 0
+            xAux = x[i] - alpha * t
+            if xAux >=0 and xAux < 0.2:
+                y[i] = m.exp(-m.log(50,m.e) * ((((xAux) - 0.15)/0.05) ** 2))
+            elif xAux > 0.3 and xAux < 0.4:
                 y[i] = 1
-            elif x[i] > 0.5 and x[i] < 0.55:
-                y[i] = 20 * (x[i] - alpha * t) - 10
-            elif x[i] > 0.55 and x[i] < 0.66:
-                y[i] = -20 * (x[i] - alpha * t) + 12
-            elif x[i] > 0.7 and x[i] < 0.8:
-                y[i] = m.sqrt(1 - (((x[i] - alpha * t) - 0.75) / 0.05) ** 2)
-            else:
-                y[i] = 0
+            elif xAux > 0.5 and xAux < 0.55:
+                y[i] = 20 * (xAux) - 10
+            elif xAux >= 0.55 and xAux < 0.66:
+                y[i] = -20 * (xAux) + 12
+            elif xAux > 0.7 and xAux < 0.8:
+                y[i] = m.sqrt(1 - (((xAux) - 0.75) / 0.05) ** 2)
+
     elif case == 2:
         for i in range(len(x)):
-            if x[i] >= 0 and x[i] <= 0.2:
+            xAux = x[i] - alpha * t
+            if xAux >= 0 and xAux <= 0.2:
                 y[i] = 1
-            elif x[i] > 0.2 and x[i] <= 0.4:
-                y[i] = 4 * (x[i] - alpha * t) - 0.6
-            elif x[i] > 0.4 and x[i] <= 0.6:
-                y[i] = -4 * (x[i] - alpha * t) + 2.6
-            elif x[i] > 0.6 and x[i] <= 0.8:
+            elif xAux > 0.2 and xAux <= 0.4:
+                y[i] = 4 * (xAux) - 0.6
+            elif xAux > 0.4 and xAux <= 0.6:
+                y[i] = -4 * (xAux) + 2.6
+            elif xAux > 0.6 and xAux <= 0.8:
                 y[i] = 1
             else:
                 y[i] = 0
-    
+
     return y
-
-"""
-/*
-    Método Lax–Friedrichs - LxF
-*/
-"""
-"""
-def laxFriedrichs(uu,Nx,Nt,h,dt):
-    u = np.empty([Nx+1,Nt+1])
-    
-    #Será criado 2 pontos extras que repetem o valor do contorno para tratar
-    #o problema de índices nos extremos
-    u = np.empty([Nx+3,Nt+1])
-    u[1:Nx+2,:] = uu[:,:]
-    u[0,0] = uu[-1,0]
-    u[-1,0] = uu[0,0]
-
-    for t in range(0,Nt):
-        for i in range(1,Nx+2):
-            u[i,t+1] = 0.5*(u[i+1,t]+u[i-1,t]) - (K*dt/(2.0*h))*(0.5*u[i+1,t]**2-0.5*u[i-1,t]**2)
-        #Copia os valores do contorno para os pontos extras criados
-        u[0,t+1] = u[Nx+1,t+1]
-        u[Nx+2,t+1] = u[1,t+1]
-    
-    return u[1:Nx+2,:],u
-"""
 
 def fou(uu, Nx, Nt, h, dt, alpha):
     u = np.empty([Nx+1,Nt+1])
     u[:,:] = uu[:,:]
-    
+
     for t in range(0,Nt):
-        for i in range(0,Nx+1):
+        for i in range(1,Nx):
             u[i,t+1] = u[i,t] - (dt/h*alpha) * ( facef(u,i,t,alpha) - faceg(u,i,t,alpha))
-    
+
     return u
-    
-            
+
+def topus(uu, Nx, Nt, h, dt, alpha):
+    u = np.empty([Nx+1,Nt+1])
+    u[:,:] = uu[:,:]
+    for t in range(0,Nt):
+        for i in range(1,Nx):
+            ux = u[i,t] - (dt/h*alpha) * ( facef(u,i,t,alpha) - faceg(u,i,t,alpha))
+            if ux >= 0 and ux <= 1:
+                u[i,t+1] = (2*(ux**4) - 3*(ux**3) + 2*(ux))*(u[i + 1,t] - u[i - 1,t]) + u[i - 1,t]
+            else:
+                u[i,t+1] = ux
+
+    return u
+
 def facef(u,i,t,alpha): # i + 1/2
     if alpha > 0:
         return u[i,t]
@@ -82,7 +70,7 @@ def faceg(u,i,t,alpha): # i - 1/2
         return u[i-1,t]
     else:
         return u[i,t]
-    
+
 ############################################################
 
 def condicaoInicial(Nx, Nt, h, x, case):
@@ -90,7 +78,7 @@ def condicaoInicial(Nx, Nt, h, x, case):
     if case == 1:
         for i in range(Nx+1):
             if x[i] < 0.2:
-                u[i,0] = m.exp(-m.log(50,10) * (((x[i] - 0.15)/0.05) ** 2))
+                u[i,0] = m.exp(-m.log(50,m.e) * (((x[i] - 0.15)/0.05) ** 2))
             elif x[i] > 0.3 and x[i] < 0.4:
                 u[i,0] = 1
             elif x[i] > 0.5 and x[i] < 0.55:
@@ -125,7 +113,7 @@ def condicoesContorno(u,Nx,Nt):
 
 #Entrada
 case = 2 # case1 ou case2
-alpha = 1
+alpha = 1.0 # coeficiente convectivo
 if case == 1:
     T = 1 #Tempo final case1 = 1 | case2 = 0.25
     A = 0 #Limite inferior para x
@@ -134,43 +122,43 @@ elif case == 2:
     T = 0.25 #Tempo final case1 = 1 | case2 = 0.25
     A = -1 #Limite inferior para x
     B = 1 #Limite superior para x
-    
+
 
 Nx = 400 #Quantidade de elementos
 h = (B - A)/np.float(Nx) #Discretização no espaço
-dt = 0.00025 #Discretização no tempo s δt = 2.5 × 10−4 e δt = 2.5 × 10-3
+dt = 0.0025 #Discretização no tempo s δt = 2.5 × 10−4 e δt = 2.5 × 10-3
 Nt = np.int(T/dt) #Quantidade de iterações no tempo
 x = np.linspace(A,B,Nx+1) #Para plot das aproximações
-K = 1 #Coeficiente convectivo
-tj = 0.05 #Instante de tempo desejado
+tj = 0.1 #Instante de tempo desejado
 t = int(tj/dt) #Índice correspondente ///
 
+print(alpha*dt/h)
 xe = np.linspace(A,B,1000) #Para plot da solução exata
 u_e = exata(xe, alpha, tj, case)
 
 u = condicaoInicial(Nx,Nt,h,x,case)
 u = condicoesContorno(u,Nx,Nt)
 
-u_fou = fou(u,Nx,Nt,h,dt,alpha)
+u_fou = topus(u,Nx,Nt,h,dt,alpha)
 #u_laxFri,teste = laxFriedrichs(u,Nx,Nt,h,dt)
 #u_kt,teste2 = KT(u,Nx,Nt,h,dt,t)
 
 #Plota o grafico
-plt.title('Exata x Aproximada ('+ str(Nx) +  ' elementos , dt = ' + str(dt/h) + 'h)')
-#plt.xlim (-1 ,1)
+plt.title('Exata x Aproximada ('+ str(Nx) +  ' elementos , dt = ' + str(dt))
+if case == 1:
+    plt.xlim (A ,B)
 #plt.ylim (0 ,1.1)
 plt.grid()
 
 plt.plot(xe, u_e, 'r-', label = 'Exata')
 plt.plot(x, u_fou[:,t], 'bx', label = 'First Order UpWind')
-#plt.plot(x,u_laxFri[:,t],'bx',label = 'Lax-Friedrichs')
-#plt.plot(x,u_laxFri[:,0],'kx-',label = 'Lax-inicial')
-
 
 plt.xlabel( 'x' )
 plt.ylabel ( 'u' )
 plt.legend(loc='best')
 diretorio = ""
-nomefig = 'time'+str(t)+'-lax.png'
+dtAux = str(dt/h)
+dtAux = dtAux.replace('.','-')
+nomefig = 'FOU_' + dtAux + '_case_' + str(case) + '.png'
 plt.savefig(diretorio+nomefig, dpi=200)
 plt.show()
